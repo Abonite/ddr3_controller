7d5bd895e1502f89e24131276242f8e8dc3e484e
Abonite <378751910@qq.com>
2025-08-13 17:51:04 +0800

    use sigasi

--- hdl/powerup_reset.vhd
+++ hdl/powerup_reset.vhd
@@ -17,7 +17,7 @@ entity powerup_reset is
 
         o_reset_n           : out std_logic := '0';
         o_cke               : out std_logic := '0';
-        o_cs                : out std_logic := '0';    
+        o_cs_n              : out std_logic := '1';    
         o_reset_finished    : out std_logic := '0'
     );
 end powerup_reset;
@@ -32,108 +32,143 @@ architecture Behavioral of powerup_reset is
     constant RESET_COUNT_MAX : unsigned(16 downto 0) := to_unsigned(integer(t_RESET / CLK_PERIOD), 17);
     constant CKE_COUNT_MAX : unsigned(16 downto 0) := to_unsigned(integer(t_CKE / CLK_PERIOD), 17);
     constant RECK_COUNT_MAX : unsigned(17 downto 0) := to_unsigned(integer(500 us / CLK_PERIOD), 18);
-   
-    signal curr_state : std_logic_vector := REST;
-    signal next_state : std_logic_vector := REST;
+
+    signal r_curr_state : std_logic_vector := REST;
+    signal r_next_state : std_logic_vector := REST;
     
-    signal reset_counter : unsigned(16 downto 0) := (others => '0');
-    signal reclk_counter : unsigned(17 downto 0) := (others => '0');
+    signal r_reset_counter : unsigned(16 downto 0) := (others => '0');
+    signal r_reclk_counter : unsigned(17 downto 0) := (others => '0');
+    signal r_reset_n: std_logic := '0';
+    signal r_cke: std_logic := '0';
+    signal r_cs_n: std_logic := '1';
 begin
     process (clk) begin
         if rising_edge(clk) then
             if (i_resetting = '1') then
-                curr_state <= INIT;
+                r_curr_state <= INIT;
             else
-                curr_state <= next_state;
+                r_curr_state <= r_next_state;
             end if;
         else
-            curr_state <= curr_state;
+            r_curr_state <= r_curr_state;
         end if;
     end process;
     
     process (all) begin
-       case curr_state is 
+        case r_curr_state is 
             when INIT =>
-                if (i_resetting) then
-                    next_state <= REST;
+                if (i_resetting = '1') then
+                    r_next_state <= REST;
                 else
-                    next_state <= INIT;
+                    r_next_state <= INIT;
                 end if;
             when REST =>
-                if reset_counter < RESET_COUNT_MAX then
-                    next_state <= REST;
+                if (r_reset_counter < RESET_COUNT_MAX) then
+                    r_next_state <= REST;
                 else
-                    next_state <= RECK;
+                    r_next_state <= RECK;
                 end if;
             when RECK =>
+                if (r_reclk_counter < RECK_COUNT_MAX) then
+                    r_next_state <= RECK;
+                else
+                    r_next_state <= CREG;
+                end if;
             when CREG =>
             when FINS =>
             when others =>
-                next_state <= INIT;
+                r_next_state <= INIT;
         end case; 
     end process;
 
     process(clk) begin
         if rising_edge(clk) then
-            case curr_state is
+            case r_curr_state is
                 when INIT =>
-                    o_reset_n <= '0';
+                    r_reset_n <= '0';
                 when REST =>
-                    o_reset_n <= '0';
+                    r_reset_n <= '0';
                 when others =>
-                    o_reset_n <= '1';
+                    r_reset_n <= '1';
             end case;
-        end if;
-    end process;
-
-    process(clk) begin
-        if rising_edge(clk) then
-            case curr_state is
-                when INIT =>
-                    o_cke <= '1';
-                when REST =>
-                    if reset_counter < (RESET_COUNT_MAX - CKE_COUNT_MAX) then
-                        o_cke <= '1';
-                    else
-                        o_cke <= '0';  -- Set CKE high after reset period
-                    end if;
-                when others =>
-                    o_cke <= '0';
-            end case;
-        end if;
-    end process;
-
-    process(clk) begin
-        if rising_edge(clk) then
-            case curr_state is
-                when REST =>
-                    if reset_counter < RESET_COUNT_MAX then
-                        reset_counter <= reset_counter + 1;
-                    else
-                        reset_counter <= (others => '0');
-                    end if;
-                when others =>
-                    reset_counter <= (others => '0');
-            end case; 
         else
-            reset_counter <= reset_counter;
+            r_reset_n <= r_reset_n;
         end if;
     end process;
 
     process(clk) begin
         if rising_edge(clk) then
-            case curr_state is
+            case r_curr_state is
+                when INIT =>
+                    r_cke <= '1';
+                when REST =>
+                    if r_reset_counter < (RESET_COUNT_MAX - CKE_COUNT_MAX) then
+                        r_cke <= '1';
+                    else
+                        r_cke <= '0';  -- Set CKE high after reset period
+                    end if;
                 when RECK =>
-                    if reclk_counter < RECK_COUNT_MAX then
-                        reclk_counter <= reclk_counter + 1;
+                    r_cke <= '0';
+                when CREG =>
+                    r_cke <= '1';
+                when others =>
+                    r_cke <= '0';
+            end case;
+        else
+            r_cke <= r_cke;
+        end if;
+    end process;
+
+    process(clk) begin
+        if rising_edge(clk) then
+            case r_curr_state is
+            when INIT =>
+                r_cs_n <= '1';
+            when REST =>
+                r_cs_n <= '1';
+            when RECK =>
+                r_cs_n <= '1';
+            when CREG =>
+                r_cs_n <= '0';
+            when others =>
+                r_cs_n <= '0';
+            end case;
+        else
+            r_cs_n <= r_cs_n;
+        end if;
+    end process;
+
+    process(clk) begin
+        if rising_edge(clk) then
+            case r_curr_state is
+                when REST =>
+                    if (r_reset_counter < RESET_COUNT_MAX) then
+                        r_reset_counter <= r_reset_counter + 1;
                     else
-                        reclk_counter <= (others => '0');
+                        r_reset_counter <= (others => '0');
                     end if;
                 when others =>
-                    reclk_counter <= (others => '0');
+                    r_reset_counter <= (others => '0');
             end case; 
         else
-            reclk_counter <= reclk_counter;
+            r_reset_counter <= r_reset_counter;
+        end if;
+    end process;
+
+    process(clk) begin
+        if rising_edge(clk) then
+            case r_curr_state is
+                when RECK =>
+                    if r_reclk_counter < RECK_COUNT_MAX then
+                        r_reclk_counter <= r_reclk_counter + 1;
+                    else
+                        r_reclk_counter <= (others => '0');
+                    end if;
+                when others =>
+                    r_reclk_counter <= (others => '0');
+            end case; 
+        else
+            r_reclk_counter <= r_reclk_counter;
         end if;
     end process;
 end architecture;
