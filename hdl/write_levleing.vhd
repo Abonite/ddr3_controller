22e4dc2e5315f52ade008488d6dc9c1c78b0f69f
Abonite <378751910@qq.com>
2025-10-04 17:59:15 +0800

    new file:   hdl/write_levleing.vhd

---
+++ hdl/write_levleing.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity write_levleing is
+    port(
+        clk : in std_logic;
+        
+        start_write_leveling : in std_logic
+    );
+end entity write_levleing;
+
+architecture Behavioral of write_levleing is
+    
+begin
+    
+end architecture Behavioral;
+
